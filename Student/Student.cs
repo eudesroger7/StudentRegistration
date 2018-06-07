bdef848c1a404d2f31bde6d4bf7e6e041ac35ab9
Eudes Roger <eudesroger7@gmail.com>
2018-06-06 23:10:06 -0300

    Create Student class

---
+++ Student/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        private string name;
+        private long matriculation;
+        private char serie;
+        private char classId;
+        private string teacher;
+        private double note1;
+        private double note2;
+        private double note3;
+        private double note4;
+        private double finalNote;
+
+        public Student()
+        {
+
+        }
+
+        //getters
+        public string getName() { return this.name; }
+        public long getMatriculation() { return this.matriculation; }
+        public char getSerie() { return this.serie; }
+        public char getClassId() { return this.classId; }
+        public string getTeacher() { return this.teacher; }
+        public double getNote1() { return this.note1; }
+        public double getNote2() { return this.note2; }
+        public double getNote3() { return this.note3; }
+        public double getNote4() { return this.note4; }
+        public double getFinalNote() { return this.finalNote; }
+
+        //setters
+        public void setName(string name) { this.name = name; }
+        public void setMatriculation(long matriculation) { this.matriculation = matriculation; }
+        public void setSerie(char serie) { this.serie = serie; }
+        public void setClassId(char classId) { this.classId = classId; }
+        public void setTeacher(string teacher) { this.teacher = teacher; }
+        public void setNote1(double note) { this.note1 = note; }
+        public void setNote2(double note) { this.note2 = note; }
+        public void setNote3(double note) { this.note3 = note; }
+        public void setNote4(double note) { this.note4 = note; }
+        public void setFinalNote(double note) { this.finalNote = note; }
+
+    }
+}
